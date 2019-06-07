71ec9650da983c721928df06e518793b22557db4
ryuto.baba <photoman1995@gmail.com>
2019-06-07 06:44:23 +0000

    kadai-tasklist

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < ApplicationRecord
     
-  validates :content, presence: true, length: { maximum: 255 }
+  validates :content, presence: true, length: { maximum: 10 }
   validates :status, presence: true, length: { maximum: 10 }
     
 end
